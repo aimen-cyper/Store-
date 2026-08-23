04993daa039eb47d309d3ab30dda1110b9a94bf4
aimen-cyper <alafdltwb@gmail.com>
2026-08-23 19:04:48 +0300

    feat: mount verified payment webhook

--- apps/api/dhat_store/app.py
+++ apps/api/dhat_store/app.py
@@ -1,4 +1,6 @@
 from .main import app
 from .management_routes import router as management_router
+from .payment_routes import router as payment_router
 
 app.include_router(management_router)
+app.include_router(payment_router)
